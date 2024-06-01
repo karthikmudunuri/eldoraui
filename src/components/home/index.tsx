5d8e690f1f16df28066fe24f133504230a3429a8
Mudunuri bhaskara karthikeya varma <102793643+karthikmudunuri@users.noreply.github.com>
2024-06-01 22:57:45 +0530

    Update index.tsx

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,13 +1,13 @@
-import { About } from './about';
+
 import { CTA } from './cta';
 import { Hero } from './hero';
-import { Showcase } from './showcase';
+
 
 export function Home(): JSX.Element {
   return (
     <main className="space-y-40 mb-40">
       <Hero />
-      <About />
+     
      
       <CTA />
     </main>
