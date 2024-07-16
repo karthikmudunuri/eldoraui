d6233aeba669894154944df53cf599e5c753c00f
Mudunuri bhaskara karthikeya varma <102793643+karthikmudunuri@users.noreply.github.com>
2024-07-17 00:20:03 +0530

    Delete src directory

--- src/components/home/container.tsx
+++
@@ -1,6 +0,0 @@
-export function Container({ children }: { children: React.ReactNode }) {
-        return (
-            <div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">{children}</div>
-        );
-    }
-  
--- src/components/icons/README.md
+++
@@ -1,7 +0,0 @@
-# Icons
-
-We use `lucide-react` for our icons. If you need an icon that is not available, please add it as a new component in `components/icons`.
-
-Sources:
-
-- https://reactsvgicons.com/
