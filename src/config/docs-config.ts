99cd403371feb8936b722707d8bc6ac9a3b467f7
Mudunuri bhaskara karthikeya varma <102793643+karthikmudunuri@users.noreply.github.com>
2024-07-25 01:08:01 +0530

    Update docs-config.ts

--- src/config/docs-config.ts
+++ src/config/docs-config.ts
@@ -72,6 +72,7 @@ export const docsConfig: DocsConfig = {
         slug: "templates",
         items: [
           { title: "portfolio", href: "/templates/portfolio" },
+          { title: "Saas Landing", href: "/templates/saas-landing" },
          
          
         ]
