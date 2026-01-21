a9485a555e584d1a50c8d6ed61a557d7b3a3e819
Maxim Bortnikov | מקסים בורטניקוב <43994704+Northstrix@users.noreply.github.com>
2026-01-21 22:55:08 +0700

    Change framer-motion for motion

--- apps/www/registry/eldoraui/holographic-card.tsx
+++ apps/www/registry/eldoraui/holographic-card.tsx
@@ -7,7 +7,7 @@ import {
   useMotionValue,
   useSpring,
   useTransform,
-} from "framer-motion"
+} from "motion"
 
 // ==========================================
 // ==== Component Interface ================
